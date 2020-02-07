629658d42ca5341b66ce72bff0a303e1b4838965
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2020-02-07 14:45:13 +0200

    most of the thumbnail-updated

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 
 class Projects extends Component {
@@ -15,13 +16,13 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://altaclean.net/wp-content/uploads/2017/01/Commercial-Cleaning-Company-Dallas-300x200.jpg) center / cover'}} >CleanNow</CardTitle>
+            <CardTitle style={{color: '#fff', height: '200px', background: 'url(http://bikashxsharma.com/projects/thumbnails/clean_now_thumbnail.png) center / cover'}} >CleanNow</CardTitle>
             <CardText>
               A site designed completely using HTML & CSS. I used combination of CSS grid and flex to make it responsive.
             </CardText>
             <CardActions className="card-action" border>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/cleaning-company", "_blank")}>GitHub</Button>
+              <Button colored onClick={()=> window.open("http://bikashxsharma.com/projects/cleaning-company/", "_blank")}>Live Demo</Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -30,13 +31,17 @@ class Projects extends Component {
 
           {/* Project 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Entertainment Portal</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/canadanepal_thumbnail.png) center / cover'}} >Entertainment Portal</CardTitle>
             <CardText>
             Entertainment portal website designed completely using HTML & CSS. Later to be converted to React.
             </CardText>
             <CardActions className="card-action" border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
+             
+             <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/canadanepal", "_blank")}>GitHub</Button>
+            
+              
+              <Button colored onClick={()=> window.open("http://bikashxsharma.com/projects/canadanepal/", "_blank")}>Demo</Button>
+             
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -51,13 +56,13 @@ class Projects extends Component {
         <div>
               {/* Project 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Massage Clinic</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/massage_thumbnail.png) center / cover'}} >Massage Clinic</CardTitle>
             <CardText>
             A responsive single page website for a massage clinic in Helsinki.
             </CardText>
             <CardActions className="card-action" border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
+             
+              <Button colored onClick={()=> window.open("https://hiraethhieronta.fi/", "_blank")}>Demo</Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -65,13 +70,13 @@ class Projects extends Component {
           </Card>
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Web Design Agency</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/velkko_thumbnail.png) center / cover'}} >Web Design Agency</CardTitle>
             <CardText>
             This is a website project for a landing page of web design company.
             </CardText>
             <CardActions className="card-action" border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
+             
+              <Button colored onClick={()=> window.open("https://velkko.fi", "_blank")}>Demo</Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -84,11 +89,98 @@ class Projects extends Component {
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>JavaScript</h1></div>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/drawing_thumbnail.png) center / cover'}} >Drawing App</CardTitle>
+            <CardText className="dark-title">
+              A site designed completely using HTML & CSS. I used combination of CSS grid and flex to make it responsive.
+            </CardText>
+            <CardActions className="card-action" border>
+              <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/canvasDrawApp", "_blank")}>GitHub</Button>
+              <Button colored onClick={()=> window.open("http://bikashxsharma.com/projects/canvasDrawApp/", "_blank")}>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/issue_tracker_thumbnail.png) center / cover'}} >Ticket Issue Tracker</CardTitle>
+            <CardText>
+            Entertainment portal website designed completely using HTML & CSS. Later to be converted to React.
+            </CardText>
+            <CardActions className="card-action" border>
+             
+             <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/ticket-issue-tracker", "_blank")}>GitHub</Button>
+            
+              
+              <Button colored onClick={()=> window.open("http://bikashxsharma.com/projects/ticket-issue-tracker/", "_blank")}>Demo</Button>
+             
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        {/* Project 3 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/guess_thumbnail.png) center / cover'}} >Number Guessing Game</CardTitle>
+                    <CardText>
+                    Entertainment portal website designed completely using HTML & CSS. Later to be converted to React.
+                    </CardText>
+                    <CardActions className="card-action" border>
+                    
+                    <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/guess-number", "_blank")}>GitHub</Button>
+                    
+                      
+                      <Button colored onClick={()=> window.open("https://bikashxsharma.com/projects/guess-number/", "_blank")}>Demo</Button>
+                    
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                      <IconButton name="share" />
+                    </CardMenu>
+                  </Card>
+
+        </div>
       )
     } else if(this.state.activeTab === 3) {
       return (
-        <div><h1>React</h1></div>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/birdwatch_thumbnail.png) center / cover'}} >Bird Watch App</CardTitle>
+            <CardText>
+            This is an App to track/report bird's information. Whenever people see a bird they can report about the bird.
+            </CardText>
+            <CardActions className="card-action" border>
+              <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/bird-watch-app", "_blank")}>GitHub</Button>
+              <Button colored >Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://bikashxsharma.com/projects/thumbnails/guess_thumbnail.png) center / cover'}} >Portfolio App</CardTitle>
+            <CardText>
+            This is a portfolio site done in React. Idea is to keep the project grow utilizing the benefits of Component.
+            </CardText>
+            <CardActions className="card-action" border>
+             
+             <Button colored onClick={()=> window.open("https://github.com/bikashxsharma/ticket-issue-tracker", "_blank")}>GitHub</Button>
+            
+              
+              <Button colored >Demo</Button>
+             
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          </div>
       )
     }
     else if(this.state.activeTab === 4) {
